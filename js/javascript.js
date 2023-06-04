6cdb42ba51798f09a83179159704ce3dbdf6ea8f
Jan Kolařík <kolarikjan02@gmail.com>
2023-06-04 12:39:41 +0200

    theme done

--- js/javascript.js
+++ js/javascript.js
@@ -4,12 +4,29 @@ $(document).ready(function () {
     Fancybox.bind("[data-fancybox]", {});
 
     $('.owl-homepage').owlCarousel({
-        loop:true,
-        nav:true,
-        navText: ["<img src='img/arrow-banner-left.png' alt='arrow' class='partners-slider'><img src='img/arrow-banner-right.png' alt='arrow' class='partners-slider-hover'>","<img src='img/arrow-banner-left.png' alt='arrow' class='partners-slider'><img src='img/arrow-banner-right.png' alt='arrow' class='partners-slider-hover'>"],
+        loop:false,
+        rewind:true,
+        nav:false,
         dots: false,
         margin:1,
         items:1,
+    });
+
+    $('.owl-news').owlCarousel({
+        loop:true,
+        nav:true,
+        navText: ["<img src='img/arrow-slider-red.png' alt='arrow'>","<img src='img/arrow-slider-red.png' alt='arrow'>"],
+        dots: true,
+        margin:30,
+        items:1,
+        responsive: {
+            768: {
+                items: 2
+            },
+            1200: {
+                items: 3
+            },
+        }
     });
 
 });
